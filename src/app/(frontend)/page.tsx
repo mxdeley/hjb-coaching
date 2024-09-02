29763930395702f1f650891a3f9280d8843c333c
mxdeley <dev@madeleydesignstudio.org>
2024-09-02 22:41:16 +0100

    setup supabase, paycloud and next.js

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -3,9 +3,6 @@ import { Button } from '@/components/ui/button'
 export default function Home() {
   return (
     <div className="text-red-500">
-      <div>
-        <Button>Hello World</Button>
-      </div>
       <Alert>
         <AlertTitle>Hello World</AlertTitle>
         <AlertDescription>Hello World</AlertDescription>

bc0c25fd875ff4c54371703418891350f5204159
Dan <dev@madeleydesignstudio.org>
2024-09-09 22:21:24 +0100

    added react-email

---
+++ emails/HelloEmail.tsx
@@ -0,0 +1,20 @@
+// @ts-nocheck
+
+import { Button } from '@react-email/button'
+import { Html } from '@react-email/html'
+import * as React from 'react'
+
+export default function HelloEmail() {
+  return (
+    <Html>
+      <Button
+        pX={20}
+        pY={12}
+        href="https://example.com"
+        style={{ background: '#000', color: '#fff' }}
+      >
+        Hello
+      </Button>
+    </Html>
+  )
+}

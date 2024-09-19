cf6e240deee6bb123679debe80a80119e63758fe
Dan <dev@madeleydesignstudio.org>
2024-09-19 23:48:40 +0100

    added openpanel

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react'
 import { cn } from '@/lib/utils'
 import { Inter as FontSans } from 'next/font/google'
+import { OpenPanelComponent } from '@openpanel/nextjs'
 
 type LayoutProps = {
   children: ReactNode
@@ -19,6 +20,14 @@ const Layout = ({ children }: LayoutProps) => {
     <html>
       <body className={cn('min-h-screen  font-sans antialiased', fontSans.variable)}>
         <FathomAnalytics />
+        <OpenPanelComponent
+          clientId="fc0b7341-c9e3-49bb-a867-763f4c2d7112"
+          trackScreenViews={true}
+          trackAttributes={true}
+          trackOutgoingLinks={true}
+          // If you have a user id, you can pass it here to identify the user
+          // profileId={'123'}
+        />
         {children}
       </body>
     </html>
